c05eb1cff80ba00928268f177176e9f00c02f474
RickyPal <rickypalomares@gmailcom>
2015-05-15 14:29:03 -0700

    Update Parachute.ino

--- TVCSupportLibrary/Parachute.ino
+++ TVCSupportLibrary/Parachute.ino
@@ -1,3 +1,21 @@
-void deploy() {
-  digitalWrite(EJECTIONCHARGEPIN);
+//Parachute Sequence
+// Deploy bit = 2
+//button pin = 4
+//xbee on channel 1337
+
+const int button = 8;
+int buttonState = 0;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(button, INPUT);
+//RADIO
+Serial.begin (9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if (button == LOW) {
+  Serial.println (2);
+  delay (5);
+}
 }
